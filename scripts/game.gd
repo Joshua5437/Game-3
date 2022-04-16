23a18013dc458576fb95f2ea6f014bc9f02a17ba
Henry Nhan <shadowhen89@gmail.com>
2022-04-15 22:11:56 -0500

    implemented basic scene w/ placing towers and camera movement

---
+++ scripts/game.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+onready var map = $Map
+
+# Used to place the towers on the map
+export(PackedScene) var tower
+
+func _process(delta):
+	# Get global mouse position
+	var global_mouse_position = get_global_mouse_position()
+	
+	# Places tower on the map if mosue left button is pressed
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) \
+		and map.is_building_there(global_mouse_position):
+		map.place_building(global_mouse_position, tower)
+		print("placed tower")

5149a78bbd3ff7b18d02a5772e21385dd23c6676
Henry Nhan <shadowhen89@gmail.com>
2022-04-19 00:08:22 -0500

    added tower shooting

---
+++ bullet.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+export(float) var speed = 100
+var direction = Vector2()
+
+func _process(delta):
+	# Moves the bullet in a specified direction and specifed speed
+	position += speed * direction * delta
+
+func _on_Bullet_body_entered(body):
+	# Despawns after hiting the enemy
+	if body.is_in_group("enemies"):
+		queue_free()
+
+func _on_Lifetime_timeout():
+	queue_free()
--- scripts/tower.gd
+++ scripts/tower.gd
@@ -6,7 +6,36 @@ extends Node2D
 # - Needs better texture
 # - Need to look at how Godot scene are inherited
 
-func _on_Range_area_entered(area):
-	# Used to check other areas (mostly towers and enemies)
-	# Note: Please look at the documentation for Area2D to get overlapping areas
-	print(area)
+onready var range_area = $Range
+onready var reload_timer = $ReloadTimer
+
+export(PackedScene) var bullet
+
+var tracked_enemies = []
+var current_target : Node2D = null
+
+func _process(delta):
+	# Shoot at the target, if it exists
+	if current_target != null and reload_timer.is_stopped():
+		shoot(current_target.position)
+		reload_timer.start()
+
+# Shoots at the position
+# target: target position
+func shoot(target : Vector2):
+	var new_bullet = bullet.instance()
+	
+	new_bullet.position = position
+	new_bullet.direction = global_position.direction_to(target)
+	get_parent().add_child(new_bullet)
+
+func _on_Range_body_entered(body):
+	# Adds enemy to list of tracked enemies
+	if body.is_in_group("enemies"):
+		tracked_enemies.push_back(body)
+		current_target = body
+
+func _on_Range_body_exited(body):
+	# Removes enemy from list of track enemies
+	if body.is_in_group("enemies"):
+		tracked_enemies.erase(body)

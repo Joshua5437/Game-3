072c9d6cc4a4e47cbe73715a28c91a346307f54f
Henry Nhan <shadowhen89@gmail.com>
2022-04-27 12:37:07 -0500

    made build sound non-positional and fixed enemy sound effect stopping playing

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -131,8 +131,23 @@ func generate_new_path():
 func damage(hits):
 	health -= hits
 	if health <= 0 and not dead:
+		# Set one-shot death to be true (prevent going through death code twice)
 		dead = true
+		
+		# Stops the enemy from processing
+		pause_mode = Node.PAUSE_MODE_STOP
+		
+		# Hides the enemy from view
+		hide()
+		
+		# Emits signal for enemy death
 		emit_signal("die", self)
+		
+		# Waits for the attack sound to finish if playing
+		if attack_sound.playing:
+			yield(attack_sound, "finished")
+		
+		# Set the node on a queue to be free
 		queue_free()
 
 func _on_building_destruction():

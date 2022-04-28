a0d8c84aeb26392e9e289653a2e70db12d437f82
Henry Nhan <shadowhen89@gmail.com>
2022-04-28 00:26:31 -0500

    added grass and grass-water textures, made grass pretty

---
+++ scripts/global.gd
@@ -0,0 +1,9 @@
+extends Node
+
+# Returns unique id using cantor pairing function
+func cantor(x, y):
+	return (x + y) * (x + y + 1) / 2 + y
+
+# Returns unqiue id using cantor pairing function
+func cantorv(vec : Vector2):
+	return (vec.x + vec.y) * (vec.x + vec.y + 1) / 2 + vec.y

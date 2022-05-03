ca173cbf2246eeaf2fba6d7b723424fc2fbfffe8
Henry Nhan <shadowhen89@gmail.com>
2022-05-03 13:58:33 -0500

    when hovering over a tower, range will be shown

--- buildings/tower/tower.gd
+++ buildings/tower/tower.gd
@@ -7,6 +7,7 @@ export(PackedScene) var bullet
 
 var tracked_enemies = []
 var current_target : Node2D = null
+var show_range = false
 
 func _process(delta):
 	# Picks a target to shoot if the target does not exist
@@ -18,8 +19,31 @@ func _process(delta):
 		shoot(current_target.position)
 		reload_timer.start()
 
+
+func _draw():
+	if show_range:
+		# Center has to be local to the position of the tower
+		draw_circle_arc(Vector2(0,0), get_weapon_range(), 0, 360, Color.green)
+
+
+# Draws circle arc without filling in the circle
+func draw_circle_arc(center, radius, angle_from, angle_to, color):
+	var nb_points = 32
+	var points_arc = PoolVector2Array()
+
+	# Calculate all points on the circle arc
+	for i in range(nb_points + 1):
+		var angle_point = deg2rad(angle_from + i * (angle_to-angle_from) / nb_points - 90)
+		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+	
+	# Draws the circle arc
+	for index_point in range(nb_points):
+		draw_line(points_arc[index_point], points_arc[index_point + 1], color)
+
+
 func get_weapon_range():
 	return $Range/CollisionShape2D.shape.radius
+
 
 # Shoots at the position
 # target: target position
@@ -50,14 +74,18 @@ func _remove_enemy(enemy : Node2D):
 	if not tracked_enemies.empty():
 		current_target = tracked_enemies[0]
 
-func _on_Range_body_entered(body):
-	_add_enemy(body)
-
-func _on_Range_body_exited(body):
-	_remove_enemy(body)
-
 func _on_Range_area_entered(area):
 	_add_enemy(area)
 
 func _on_Range_area_exited(area):
 	_remove_enemy(area)
+
+
+func _on_Tower_mouse_entered():
+	show_range = true
+	update()
+
+
+func _on_Tower_mouse_exited():
+	show_range = false
+	update()
